f463871b6b8ef8accc46a763c5694a7b3cf94b36
Adrian Zamora <adrianzamora10@outlook.com>
2024-01-29 16:44:42 -0700

    chore: Actualizar configuración de Firebase - Agregar la configuración de conexión a Firebase para utilizar su servicio de almacenamiento en la nube

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from 'firebase/analytics';
 
 /* eslint-disable*/
 const config = {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
-    measurementId: '',
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 }
 const firebase = Firebase.initializeApp(config);
 const analytics = getAnalytics(firebase);
