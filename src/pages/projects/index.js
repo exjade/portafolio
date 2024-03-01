2226307ae0d2f96b38264fb4e70eb43b47f5ec37
Adrian Zamora <adrianzamora10@outlook.com>
2024-02-29 17:35:54 -0700

    refactor(projects): update Projects page structure and state handling

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,8 +1,42 @@
-import React from 'react';
+import { useState } from 'react';
+import { Header, Container, Wrapper, Footer, } from '../../components';
 
 function Projects() {
+
+    
+    // Setea el estado inicial en la página de inicio
+    const [activePage, setActivePage] = useState('')
+
+
+    // Establece la página activa en el estado local
+    const handleBreadcrumbClick = (page) => {
+        setActivePage(page)
+    }
+
+
     return (
-        <div>Projects</div>
+        <Container>
+        <Wrapper >
+            <Header
+                name='Adrian Zamora'
+                contact='_contact-me'
+                home='_hello'
+                about='_about-me'
+                projects='_projects'
+                activePage={activePage}
+                handleBreadcrumbClick={handleBreadcrumbClick}
+            />
+
+            <div>acerca de mi</div>
+
+            <Footer
+                title='Find me in:'
+                github='@exjade'
+                activePage={activePage}
+                handleBreadcrumbClick={handleBreadcrumbClick}
+            />
+        </Wrapper>
+    </Container>
     )
 }
 
